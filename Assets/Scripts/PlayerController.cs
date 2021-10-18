f853d646eb2bbadac1388a93d019e46ee056e584
Gerardo Rodriguez <gerardo.rodriguezdelgado@nxp.com>
2021-10-18 18:30:08 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float moveDirection;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    //    private bool onTheGround = false;
+    private bool jumping = false;
+    private Rigidbody2D rbPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxisRaw("Horizontal");
+        jumping = Input.GetKeyDown("space");
+        if (jumping)
+        {
+            jumping = false;
+            Debug.Log("Jumping");
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Move();
+    }
+
+    void Move()
+    {
+        rbPlayer.velocity = new Vector2(moveSpeed * moveDirection, rbPlayer.velocity.y);
+    }
+
+    void Jump()
+    {
+        rbPlayer.AddForce(transform.up * jumpForce);
+    }
+}
